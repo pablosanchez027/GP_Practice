7bc0abaf96751ec06f83228b48c30c201138d016
Pablo Sánchez <pablosanchez027@hotmail.com>
2019-01-22 14:52:48 -0700

    Move and flip

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float moveSpeed = 0f;
+
+    Animator anim;
+    SpriteRenderer spr;
+
+    float moveX;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * moveX * moveSpeed * Time.deltaTime);
+
+        anim.SetFloat("MoveX", Mathf.Abs(moveX));
+
+        spr.flipX = moveX < 0f ? true : moveX > 0f ? false : spr.flipX;
+    }
+}
